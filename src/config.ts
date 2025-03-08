0f74991555715fa9a1acc71586b638d25fc067c0
seth <117851610+Sethispr@users.noreply.github.com>
2025-03-08 06:25:52 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: '',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Pola Docs',
   bio: 'The documentation site for Pola',
   links: [
